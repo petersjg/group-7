d0282316c3591649289d4c7b3544e5e8f3a9bf6e
FriendlyBandit <john.geraldo.peters@gmail.com>
2021-02-15 17:35:04 -0600

    started main method

--- Builder Lab/builderSource/builderMain.java
+++ Builder Lab/builderSource/builderMain.java
@@ -1,2 +1,16 @@
 public class builderMain {
+
+    public void main(String[] args){
+        ComputerBuilder builder500Dollar = new ComputerBuilder500Dollar();
+        ComputerBuilder builder1000Dollar = new ComputerBuilder1000Dollar();
+        ComputerBuilder builder1500Dollar = new ComputerBuilder1500Dollar();
+
+        Director director500Dollar = new Director(builder500Dollar);
+        Director director1000Dollar = new Director(builder1000Dollar);
+        Director director1500Dollar = new Director(builder1500Dollar);
+
+        
+    }
+
+
 }
