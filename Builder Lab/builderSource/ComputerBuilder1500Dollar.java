0cedbe345d556b00fffe4c91ad761d1576827afc
gaugerd <gaugerderek@gmail.com>
2021-02-15 17:26:16 -0600

    modified:   Builder Lab/builderSource/ComputerBuilder1500Dollar.java

--- Builder Lab/builderSource/ComputerBuilder1500Dollar.java
+++ Builder Lab/builderSource/ComputerBuilder1500Dollar.java
@@ -3,47 +3,47 @@ public class ComputerBuilder1500Dollar implements ComputerBuilder{
 
     @Override
     public void setCPU() {
-        computer.setCPU("High Quality CPU");
+        CPU = ("High Quality CPU");
     }
 
     @Override
     public void setGPU() {
-        computer.setGPU("High Quality GPU");
+        GPU = ("High Quality GPU");
     }
 
     @Override
     public void setPSU() {
-        computer.setPSU("High Quality PSU");
+        PSU = ("High Quality PSU");
     }
 
     @Override
     public void setCase() {
-        computer.setCase("High Quality Case");
+        Case = ("High Quality Case");
     }
 
     @Override
     public void setRAM() {
-        computer.setRAM("High Quality RAM");
+        RAM = ("High Quality RAM");
     }
 
     @Override
     public void setStorage() {
-        computer.setStorage("High Quality Storage");
+        Storage = ("High Quality Storage");
     }
 
     @Override
     public void setFans() {
-        computer.setFans("High Quality Fans");
+        Fans = ("High Quality Fans");
     }
 
     @Override
     public void setCooler() {
-        computer.setCooler("High Quality Cooler");
+        Cooler = ("High Quality Cooler");
     }
 
     @Override
     public void setMotherBoard() {
-        computer.setMotherboard("High Quality Motherboard");
+        Motherboard = ("High Quality Motherboard");
     }
 
     @Override
